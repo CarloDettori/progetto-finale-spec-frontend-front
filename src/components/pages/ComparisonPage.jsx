f72fbf211d704600a6c9b01692546f2e0a498071
CarloDettori <dettoric07@gmail.com>
2025-06-08 00:37:51 +0200

    some fix

--- src/components/pages/ComparisonPage.jsx
+++ src/components/pages/ComparisonPage.jsx
@@ -27,15 +27,15 @@ export default function ComparisonPage() {
 
     return (<>
 
-        <h1 className="pb-7.5 text-4xl text-gray-500 arcadefont"><strong>Confronta due Videogiochi</strong></h1>
-        <div className="flex justify-between pt-20 items-end">
+        <h1 className="pb-10 text-4xl text-gray-500 arcadefont"><strong>Confronta due Videogiochi</strong></h1>
+        <div className="flex justify-between  items-end">
             <h1 className="text-center text-3xl pb-4 w-max arcadefont"><strong>Videogioco 1</strong></h1>
             <h1 className="text-center text-8xl  arcadefont"><strong>VS</strong></h1>
             <h1 className="text-center text-3xl pb-4 w-max arcadefont"><strong>Videogioco 2</strong></h1>
         </div>
         <div className="flex mx-auto justify-evenly">
 
-            <div className="comparison-deck pe-12.5 my-20">
+            <div className="comparison-deck pe-12.5 my-5">
                 {selectedGameId1
                     ? <div ref={detailRef}>
                         <GameDetailComponent id={selectedGameId1} onBack={() => setSelectedGameId1(null)} />
@@ -50,7 +50,7 @@ export default function ComparisonPage() {
                 <div className=" bg-[#4a5566] h-full w-1.5 p-0"></div>
             </div>
 
-            <div className="comparison-deck ps-12.5 my-20">
+            <div className="comparison-deck ps-12.5 my-5">
                 {selectedGameId2
                     ? <div ref={detailRef}>
                         <GameDetailComponent id={selectedGameId2} onBack={() => setSelectedGameId2(null)} />
