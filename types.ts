e6e27fd0d8047b8796e44864a1954b974f591bd6
CarloDettori <dettoric07@gmail.com>
2025-06-11 17:30:27 +0200

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,12 @@
+export type Game = {
+    title: string,
+    img: string,
+    description: string,
+    category: string,
+    weightMB: number,
+    genere: string[],
+    price: number,
+    goty: boolean,
+    releaseDate: string,
+    score: number
+};
