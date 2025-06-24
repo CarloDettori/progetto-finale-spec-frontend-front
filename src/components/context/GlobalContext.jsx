3f99c81ee01dcc3da495beec818c999648528d59
CarloDettori <dettoric07@gmail.com>
2025-06-24 16:05:49 +0200

    new frontend fetch

--- src/components/context/GlobalContext.jsx
+++ src/components/context/GlobalContext.jsx
@@ -15,7 +15,7 @@ const GlobalProvider = ({ children }) => {
     }
 
     useEffect(() => {
-        fetchData("http://localhost:3001/games")
+        fetchData("public/database/games.json")
             .then(obj => setGames(obj))
             .catch(error => console.error(error))
             .finally(console.log("fetch end"))
